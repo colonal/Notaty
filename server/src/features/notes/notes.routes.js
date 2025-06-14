c37f8f031ae3c4a71aab67ca45f35e9ec77ea798
Colonal <colonal.ch@gmail.com>
2025-06-14 23:58:53 +0300

    feat(server): Add authentication middleware to notes and user routes. Secured all CRUD operations for notes and user retrieval, update, and deletion with JWT-based authentication.

--- server/src/features/notes/notes.routes.js
+++ server/src/features/notes/notes.routes.js
@@ -1,20 +1,21 @@
 import express from 'express';
+import authMiddleware from '../../middlewares/authMiddleware.js';
 import noteController from './notes.controller.js';
 const router = express.Router();
 
 // Create POST API to be able to create a new note
-router.post('/', noteController.createNote);
+router.post('/', authMiddleware, noteController.createNote);
 
 // Create GET API to be able to retrieve all notes
-router.get('/', noteController.getAllNotes);
+router.get('/', authMiddleware, noteController.getAllNotes);
 
 // Create GET API to be able to retrieve a note by ID
-router.get('/:id', noteController.getNoteById);
+router.get('/:id', authMiddleware, noteController.getNoteById);
 
 // Create PUT API to be able to update a note
-router.put('/', noteController.updateNote);
+router.put('/', authMiddleware, noteController.updateNote);
 
 // Create DELETE API to be able to delete a note
-router.delete('/:id', noteController.deleteNote);
+router.delete('/:id', authMiddleware, noteController.deleteNote);
 
 export default router;
--- server/src/features/user/user.routes.js
+++ server/src/features/user/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { isDev } from '../../config/env.js';
+import authMiddleware from '../../middlewares/authMiddleware.js';
 import { userController } from './user.controller.js';
 
 const router = express.Router();
@@ -7,9 +8,9 @@ const router = express.Router();
 
 router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/:id', userController.findUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/:id', authMiddleware, userController.findUserById);
+router.put('/:id', authMiddleware, userController.updateUser);
+router.delete('/:id', authMiddleware, userController.deleteUser);
 
 if (isDev()) {
     router.get('/', userController.findAllUsers);

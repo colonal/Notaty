0b7e4a351733a280629e9856ff2c676f75783af3
Colonal <colonal.ch@gmail.com>
2025-08-07 11:57:04 +0300

    feat(localization): Implement localization support with easy_localization package and add English translations

---
+++ app/lib/core/enum/localization.dart
@@ -0,0 +1,9 @@
+export 'package:Notaty/core/extension/localization_extension.dart';
+
+enum Localization {
+  en("En");
+
+  final String code;
+
+  const Localization(this.code);
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:Notaty/core/enum/localization.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: Localization.values.map((e) => e.locale).toList(),
+      path: 'assets/translations',
+      startLocale: Localization.en.locale,
+      fallbackLocale: Localization.en.locale,
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,26 +24,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'app_name'.tr(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: MyHomePage(title: 'app_name'.tr()),
     );
   }
 }

76a0cc343c2a9e83568285eb5e68f0990be44434
Colonal <colonal.ch@gmail.com>
2025-08-09 14:24:55 +0300

    feat: Update app icons and launch screen assets
    - Replaced existing app icons with new images for various sizes and resolutions.
    - Added new dark mode app icons for better visibility in dark themes.
    - Introduced a new launch background image and updated the launch screen storyboard to include it.
    - Updated Info.plist to reflect the new app name "Notaty".
    - Integrated flutter_native_splash for a smoother splash screen experience.
    - Added package_rename_config.yaml for easier package renaming.
    - Updated pubspec.yaml to include new dependencies and versions.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,9 +5,11 @@ import 'package:Notaty/core/services/user_services.dart';
 import 'package:Notaty/core/theming/app_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Initialize dependency injection
   configureDependencies();
@@ -17,6 +19,9 @@ void main() async {
 
   // Initialize the app route
   await getIt<AppRoute>().init(getIt<UserServices>().isAuthenticated);
+
+  // Remove the splash screen after initialization
+  FlutterNativeSplash.remove();
 
   runApp(
     EasyLocalization(
